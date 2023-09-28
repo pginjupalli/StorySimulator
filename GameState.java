3d8304601ee722765fdc0891a5f69e27fd3e4753
Pooja Ginjupalli <113191715+pginjupalli@users.noreply.github.com>
2023-09-28 16:29:07 -0400

    Created GameState.java

---
+++ GameState.java
@@ -0,0 +1,8 @@
+/**This enum represents possible game states: WIN, LOSE, NOT OVER
+ * 
+ * @author Pooja Ginjupalli
+ */
+
+public enum GameState {
+	GAME_OVER_WIN, GAME_OVER_LOSE, GAME_NOT_OVER;
+}
